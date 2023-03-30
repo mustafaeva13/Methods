189a6ee5dfc5013f929fcc0914bec8d895b63717
efsane <126017552+mustafaeva13@users.noreply.github.com>
2023-03-30 07:11:47 +0400

    Add project files.

---
+++ Methods/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+class Program
+{
+    //Task 4 thsi number odd -even
+    // Public static bool isEven(int n)
+    //{
+    //    return (n % 2 == 0);
+    //}
+
+    //public static void Main()
+    //{
+    //    int n;
+    //    Console.WriteLine("enter your number: ");
+    //    n=int.Parse(Console.ReadLine());
+    //    if (isEven(n) == true)
+    //        Console.WriteLine(" This number is Even");
+    //    else
+    //        Console.WriteLine(" This number is Odd");
+    //}
+
+
+
+    ///Task5
+
+
+    //public static void Main()
+    //{
+
+    //    bool number1= false;
+    //    bool gamer = true;
+
+
+    //    string developer = number1.ToString();
+    //    string pubg= gamer.ToString();
+
+    //    Console.WriteLine( developer);
+    //    Console.WriteLine( pubg);
+    //}
+
+
+    ///Task 6 
+
+    //static void Main(string[] args)
+    //{
+    //    string Name = "Efsane ";
+    //    string Surname = "Mstafaeva";
+    //    string name = Name + Surname;
+    //    Console.WriteLine(name);
+    //}
+
+
+
+
+    ///Task1
+    //public static int Sum(int num1, int num2)
+    //{
+    //    int sum;
+    //    sum = num1 + num2;
+    //    return sum;
+    //}
+
+    //public static void Main()
+    //{
+
+    //    Console.Write("Enter a first number: ");
+    //    int n1 = Convert.ToInt32(Console.ReadLine());
+    //    Console.Write("Enter a second  number: ");
+    //    int n2 = Convert.ToInt32(Console.ReadLine());
+    //    Console.WriteLine("\n sum of two numbers : {0} \n", Sum(n1, n2));
+    //}
+
+
+    ///Task7
+    //    static void Main(string[] args)
+    //{
+    //    int number;
+    //    Console.WriteLine("Enter your number:");
+    //    number=Convert.ToInt32(Console.ReadLine());
+    //    Console.WriteLine(number*number);
+    //}
+
+
+
+    ///Task9   50/50 
+
+
+    //public static void Main()
+    //{
+    //    int number;
+    //    int number2;
+    //    Console.Write("Your 1st number: ");
+    //    number = Convert.ToInt32(Console.ReadLine());
+    //    Console.Write("Your 2nd the number : ");
+    //    number2 = Convert.ToInt32(Console.ReadLine());
+    //    Console.WriteLine(" {0}^{1} = {2} ", number, number2, PowerRaising(number, number));
+    //}
+    //public static int PowerRaising(int a, int b)
+    //{
+    //    int r = 1;
+    //    int i;
+    //    for (i = 1; i <= b; i++)
+    //        r = r * a;
+    //    return r;
+    //}
+
+
+}
